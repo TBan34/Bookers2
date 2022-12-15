b6c50f6b65e9be2fa3ca0b5ad3739226a5d023ba
TBan34 <119559612+TBan34@users.noreply.github.com>
2022-12-15 09:10:32 +0000

    [Add]Log in, Sign upボタン（top）

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,2 +1,47 @@
 class BooksController < ApplicationController
+
+  def new
+    @book=Book.new
+  end
+
+  def create
+    @book=Book.new(book_params)
+    @book.user_id=current_user.id
+    if @book.save
+      redirect_to books_path[:id]
+    else
+      render :index
+    end
+  end
+
+  def index
+    @books=Book.all
+  end
+
+  def show
+    @book=Book.find(params[:id])
+  end
+
+  def edit
+    @book=Book.find(params[:id])
+  end
+
+  def update
+    @book=Book.find(params[:id])
+    @book.update
+    redirect_to books_path
+  end
+
+  def destroy
+    @book=Book.find(params[:id])
+    @book.destroy
+    redirect_to :index
+  end
+
+  private
+
+  def book_params
+    params.require(:book).permit(:title, :body)
+  end
+
 end
