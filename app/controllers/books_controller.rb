6657d170e60585b5db9ba44cc7bed09dc44835b1
TBan34 <119559612+TBan34@users.noreply.github.com>
2022-12-19 08:15:25 +0000

    [Fix]users/index, books_controllerのコード修正

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -19,7 +19,7 @@ class BooksController < ApplicationController
 
   def show
     @book=Book.find(params[:id])
-    @user=User.find(params[:id])
+    @user=User.find(current_user.id)
   end
 
   def edit
@@ -29,13 +29,13 @@ class BooksController < ApplicationController
   def update
     @book=Book.find(params[:id])
     @book.update
-    redirect_to books_path
+    redirect_to book_path(@book.id)
   end
 
   def destroy
     @book=Book.find(params[:id])
     @book.destroy
-    redirect_to :index
+    redirect_to books_path
   end
 
   private
