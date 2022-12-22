7d3dd7db66f154116609665e3d13c880a6ad2dff
TBan34 <119559612+TBan34@users.noreply.github.com>
2022-12-22 06:04:35 +0000

    [Finish]4-27

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,17 +1,13 @@
 class BooksController < ApplicationController
 
-  def new
-    @book=Book.new
-  end
-
   def create
+    @books=Book.all
     @book=Book.new(book_params)
     @book.user_id = current_user.id
     if @book.save
       flash[:notice]="You have created book successfully."
       redirect_to book_path(@book.id)
     else
-      @books=Book.all
       render :index
     end
   end
